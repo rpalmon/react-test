7a93ed64f06dd4113c3c9df23373ac1626c04fbf
--replace-all <--replace-all>
2020-03-28 13:37:56 +0300

    Updates

---
+++ precache-manifest.6d32a63d6048c6e07c9fb693e7e6bcdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be516f2c023e940972ebcc95b467677",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "9bb38f643a78fed1c0be",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ce84eb89da882171fa0d",
+    "url": "/react-test/static/js/2.92821ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.92821ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb38f643a78fed1c0be",
+    "url": "/react-test/static/js/main.9ba06272.chunk.js"
+  },
+  {
+    "revision": "4d564b787a9208f8dac6",
+    "url": "/react-test/static/js/runtime-main.83ddd5e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
